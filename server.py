311c6eed94c5b62d74828187e07cdb429bc47c4b
WFT <will.a.ft@gmail.com>
2014-10-04 18:09:50 -0400

    api routes

--- server.py
+++ server.py
@@ -1,9 +1,25 @@
-from flask import Flask
-import uber
+from flask import Flask, render_template, request
+import decsisions
 
 app = Flask(__name__)
 
+# Pages
+@app.route('/')
+def home():
+    return render_template('index.html')
 
+# API
+
+# the only api call you'll ever need
+# returns yes/no + weather and uber data
+# GET /api/goto/?start_lat=<latitude>&start_lon=<longitude>&end_lat=<latitude>&end_lon=<longitude>
+@app.route('/api/goto/')
+def goto():
+    params = request.args
+    origin = (params['start_lat'], params['start_lon'])
+    destination = (params['end_lat'], params['end_lon'])
+    return decision(origin, destination)
+    
 
 if __name__ == '__main__':
     app.run()
