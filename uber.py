a75a872e5cfbffca463d583ece6e8af15338d7a8
WFT <will.a.ft@gmail.com>
2014-10-04 16:06:55 -0400

    adding wunderground conditions support

--- uber.py
+++ uber.py
@@ -2,7 +2,8 @@ from tokens import UBER_SERVER_TOKEN as server_token
 import requests
 
 """
-Location tuples are expected in the form (latitude, longitude)
+- Location tuples are expected in the form (latitude, longitude)
+- uberTAXI and uberSUV for now
 """
 
 
@@ -20,3 +21,13 @@ def price(origin, destination):
     return data
 
 
+def time(location):
+    url = 'https://api.uber.com/v1/estimates/time'
+    params = {
+        'server_token':server_token,
+        'start_latitude':location[0],
+        'start_longitude':location[1] 
+    }
+    response = requests.get(url, params=params)
+    data = response.json()
+    return data
---
+++ wunderground.py
@@ -0,0 +1,10 @@
+import requests
+from tokens import WUNDERGROUND_KEY as key
+
+def conditions(state_code, city):
+    url = 'http://api.wunderground.com/api/%s/conditions'%key
+    url += '/q/%s/%s.json'%(state_code, city)
+    response = requests.get(url)
+    data = response.json()
+    return data
+    
