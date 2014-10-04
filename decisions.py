cf53b7a723e6977492c018d639f76eacad9fe3bd
WFT <will.a.ft@gmail.com>
2014-10-04 18:09:35 -0400

    starting decision making

---
+++ decisions.py
@@ -0,0 +1,14 @@
+import db
+import uber, wunderground
+import json
+
+
+def decision(origin, destination):
+    pass
+
+def get_weather():
+    weather = db.find_weather(destination)
+    if weather:
+        pass
+    else:
+        pass
