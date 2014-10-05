9164170b6b363bf209df62a5a35d6de7d7725dcc
WFT <will.a.ft@gmail.com>
2014-10-05 03:19:59 -0400

    decision making who knows what's happening it's earlier

--- decisions.py
+++ decisions.py
@@ -3,7 +3,8 @@ import uber, wunderground, gmaps
 import json
 
 MIN_WALKING_TEMP = 25
-MAX_TRANSIT_DELTA = 
+MAX_TRANSIT_DELTA = 1020
+MAX_PRICE
 
 def decision(origin, destination):
     ans = {'response':{
@@ -11,19 +12,35 @@ def decision(origin, destination):
         'uber':get_uber(origin, destination),
         'travel_time':get_transit_time(origin, destination),
     }}
-    ans['decision'] = 
+    ans['decision'] = get_decision(ans)
     return json.dumps(ans)
 
 def get_decision(data):
-    ans = True
-    if data['transit_time'] > 
-    
+    t_time = data['transit_time']
+    if 'error' in t_time['transit'] or 'error' in t_time['walking']:
+        return True
+    # t_delta is the number of seconds that non-uber will take more than uber
+    if data['weather_destination']['temp_f'] < MIN_WALKING_TEMP:
+        return True
+
+    t_delta = min(t_time['transit']['seconds'], 
+                  t_time['walking']['seconds']) - (
+                      t_time['driving']['seconds'] +
+                      data['uber']['time'][0]['estimate']
+                  )
+
+    ans = t_delta > MAX_TRANSIT_DELTA
+    ans = ans and data['uber']['time'][0]['estimate'] < MAX_TRANSIT_DELTA
+    ans = ans and 
+    return ans
 
 def get_uber(start, end):
     try:
         return {
-            'price':uber.price(start, end)['prices'],
-            'time':uber.time(start)['times']
+            'price':sorted(uber.time(start)['prices'], 
+                          key=lambda x: x['high_estimate'])
+            'time':sorted(uber.time(start)['times'], 
+                          key=lambda x: x['estimate'])
         }
     except:
         return {
@@ -59,12 +76,15 @@ def get_transit_time(origin, destination):
             return {'error':'No routes available.'}
         seconds = reduce(lambda acc, leg: acc + leg['duration']['value'],
                          routes[0]['legs'], 0)
+        return {'seconds':seconds}
     try:
         transit = gmaps.transit_duration(origin, destination, 'transit')['routes']
         walking = gmaps.transit_duration(origin, destination, 'walking')['routes']
+        driving = gmaps.transit_duration(origin, destination, 'driving')['routes']
         return {
             'public':parse_gmaps(transit),
-            'walking':parse_gmaps(walking)
+            'walking':parse_gmaps(walking),
+            'driving':parse_gmaps(driving)
         }
     except:
-        return {'error':'Transit data error'}
+        return {'error':'Data error'}
