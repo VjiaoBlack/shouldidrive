6e669a876934112b28de3e248176b2d65aa4388b
WFT <will.a.ft@gmail.com>
2014-10-05 03:20:21 -0400

    mode stuff

--- gmaps.py
+++ gmaps.py
@@ -2,13 +2,13 @@ import requests
 from tokens import GMAPS_KEY as key
 import time
 
-def transit_duration(origin, destination):
+def maps_duration(origin, destination, mode):
     url = 'https://maps.googleapis.com/maps/api/directions/json'
     params = {
         'key':key,
         'origin':'%s,%s'%origin,
         'destination':'%s,%s'%destination,
-        'mode':'transit',
+        'mode':mode,
         'departure_time':int(time.time())
     }
     return requests.get(url, params=params).json()
